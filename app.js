ce52ed037f587f43b5ec132babf2dcc85b551e7a
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-06-17 10:55:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-   
+  res.sendFile(__dirname + "/index.html");
 })
-const query = "London"
-const apiKey = "f17ac4078d2b7f96c7d3976cdefababc"
-const unit = "metric"
-//const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey+"&unit="+unit;
-    //https.get(url,function(response){
-        //console.log(response.statusCode);
-    //response.on("data",function(data){
-     //const weatherData=JSON.parse(data);
-     //const temp = weatherData.main.temp;
-     //const weatherDescription = weatherData.weather[0].description;
-     //const Icon = weatherData.weather[0].Icon;
-     //const imageUrl="http://openweathermap.org/img/wn/" + Icon +"@2x.png"
-     //res.write("<h3> the current weather is like"+ weatherDescription +"<h3>");
-     //res.write("<h1> the temperature is"+temp);
-     //res.write("<img src=" +imageUrl+  ">");
-     //res.send();
-    //})
-    //})
+app.post("/", function(req,res){
+    const query = req.body.cityName;
+    const apiKey = "f17ac4078d2b7f96c7d3976cdefababc";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +apiKey +"&units="+ unit;
+    https.get(url,function(response){
+    console.log(response.statusCode);
+    response.on("data",function(data){
+     const weatherData=JSON.parse(data);
+     const temp = weatherData.main.temp;
+     const weatherDescription = weatherData.weather[0].description;
+     const Icon = weatherData.weather[0].icon;
+     const imageUrl="http://openweathermap.org/img/wn/" + Icon +"@2x.png"
+     res.write("<h1>the temperature in " + query +" is "+ temp +" degree celcius</h1>");
+     res.write("<h3>the current weather is like "+ weatherDescription + "</h3>");
+     res.write("<img src=" + imageUrl+  ">");
+     res.send();
+    })
+    })
+});
+
 app.listen(3000,function(params){
   console.log("server running on port 3000");
 })
