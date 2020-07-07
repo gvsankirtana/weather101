97205a50da95d90b68f1a4a823dc18d814c185a4
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-07-07 10:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,24 @@ app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
 })
 app.post("/", function(req,res){
-    const query = req.body.cityName;
-    const apiKey = "f17ac4078d2b7f96c7d3976cdefababc";
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +apiKey +"&units="+ unit;
-    https.get(url,function(response){
-    console.log(response.statusCode);
-    response.on("data",function(data){
-     const weatherData=JSON.parse(data);
+    const query = req.body.cityName;//user entered name from input
+    const apiKey = "f17ac4078d2b7f96c7d3976cdefababc";//api key generated
+    const unit = "metric";//values to be shown in metrics
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +apiKey +"&units="+ unit;//to take up the values accroding to user needs
+    https.get(url,function(response){//gets the response from the url
+    console.log(response.statusCode);//show the status code to check whether it's not 404,if 200 then success message
+    response.on("data",function(data){//creates a new object
+     const weatherData=JSON.parse(data);//parse the data
+      //get the data from the api
      const temp = weatherData.main.temp;
      const weatherDescription = weatherData.weather[0].description;
      const Icon = weatherData.weather[0].icon;
      const imageUrl="http://openweathermap.org/img/wn/" + Icon +"@2x.png"
+     //to post the answer to the user side
      res.write("<h1>the temperature in " + query +" is "+ temp +" degree celcius</h1>");
      res.write("<h3>the current weather is like "+ weatherDescription + "</h3>");
      res.write("<img src=" + imageUrl+  ">");
-     res.send();
+     res.send();// to end the sending of data
     })
     })
 });
